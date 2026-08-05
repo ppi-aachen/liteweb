cb2f0189c5679a9b947b17263245ead01643d0f9
PPI Aachen <aachen.ppi@gmail.com>
2026-08-05 14:35:43 +0200

    Update Kepengurusan

--- content/pages/kepengurusan.js
+++ content/pages/kepengurusan.js
@@ -6,13 +6,13 @@ window.kepengurusanData = {
         {
             "type": "Hero",
             "title": "Kepengurusan",
-            "subtitle": "Susunan Kepengurusan PPI Aachen 2025/2026"
+            "subtitle": "Susunan Kepengurusan PPI Aachen 2026/2027"
         },
         {
             "type": "ExecutiveBoard",
-            "title": "Susunan Kepengurusan PPI Aachen 2025/2026",
-            "chair": "Adriano Ferane Gunawan",
-            "vice": "Alfred Triza & Nathanael Lumen Sukmana"
+            "title": "Susunan Kepengurusan PPI Aachen 2026/2027",
+            "chair": "Jason Louis Laksono",
+            "vice": "Dheo Santoso"
         },
         {
             "type": "DepartmentList",
@@ -21,15 +21,7 @@ window.kepengurusanData = {
                     "name": "Badan Pengawas Kegiatan dan Keuangan (BPKK)",
                     "description": "We monitor everything, making sure the captain steers the ship to the right direction.",
                     "members": [
-                        "Nathan Matthew Kurnia",
-                        "Muhammad Mulya Salam"
-                    ]
-                },
-                {
-                    "name": "Human Resources",
-                    "description": "We’re not licensed therapists 🤫. From team well-being to smooth internal coordination, they’re here to make sure PPI Aachen runs with heart, harmony, and a whole lot of care.",
-                    "members": [
-                        "Aisha Safwa Fatiha",
+                        "Ghithrafi Fadhgazka Syarif",
                         "Briggita Graciella"
                     ]
                 },
@@ -37,97 +29,41 @@ window.kepengurusanData = {
                     "name": "Sekretaris",
                     "description": "Keeping everything organized and documented.",
                     "members": [
-                        "Tresy Prawita Silalahi",
-                        "Vivian Prudence Chen",
-                        "Michelle Anwar"
+                        "Rizki Shaqi Vanilla"
                     ]
                 },
                 {
                     "name": "Bendahara",
                     "description": "Managing the finances and keeping the books balanced.",
                     "members": [
-                        "Marlyne Tanankia",
-                        "Macario Zachary Machmud"
+                        "Benedictus Warren Nugraha"
                     ]
                 },
                 {
-                    "name": "Public Relations",
-                    "description": "🚨We’re the cool kids on the block🚨. No cap, we run the socials 😎📱. From handling external comms to shaping the public image of PPI Aachen, they’re the bridge between us and the world.",
+                    "name": "Pendidikan dan Seni Budaya",
+                    "description": "Where creativity meets meaning 📚✨\n\nIntroducing the Education & Arts and Culture Department of PPI Aachen — the team that nurtures learning, culture, and creative expression in every form 🏅\nThey bring ideas to life through perspective, imagination, and thoughtful execution 🌅",
                     "members": [
-                        "Pascal Joseph Nathaniel (Ketua)",
-                        "Marie Elaine Winona Silap",
-                        "Muhammad Hanif Utomo",
-                        "Fictor Marcelbrian",
-                        "Andriyani Monica Susanto",
-                        "Rafi Arya Putra Hariyadi",
-                        "Mervin Yuan Arkana"
+                        "Ruciragati Maheswari",
+                        "Jonathan Alexander Tresna",
+                        "Jesslyn Florence"
                     ]
                 },
                 {
-                    "name": "Sports",
-                    "description": "We ball ! 🏀⚽️🔥. Meet our MVPs from the Sports Department! ⛳️🏐. From tournaments to casual games, they’re here to keep us active, build team spirit, and bring the fun (and sweat) all year long.",
+                    "name": "Hubungan Masyarakat & Multimedia",
+                    "description": "Lights, camera, connection 📸✨\nSay hello to the people behind the publications, communication, and creative face of PPI Aachen — Public Relations & Multimedia Department 🎤🎨\nFrom sharing stories to building connections, they make sure every moment and message reaches everyone in the best way possible.",
                     "members": [
-                        "Gede Pradnya Paramartha (Ketua)",
-                        "Roger Marcelbrian",
-                        "Stanislaus Joey Gunawan",
-                        "Hanif Aulia Ibrahim",
-                        "Kevin Kristianto Widjaja",
-                        "Grant Elliot Thomas",
-                        "William Krisna Sunjoto"
+                        "Shalvensky Cornelly",
+                        "Ruth Nathaline Sarumaha",
+                        "Wiratama Admaja Wahono"
                     ]
                 },
                 {
-                    "name": "Arts & Culture",
-                    "description": "Too cultured to be subtle 🔥🫦. From showcasing rich traditions to sparking creative expression, they’ll be at the heart of celebrating Indonesian culture in PPI Aachen this year.",
+                    "name": "Event Organizer",
+                    "description": "Behind every well-planned moment and seamlessly executed event 🎯✨\n\nThe team that turns ideas into structured, smooth, and memorable experiences, meet the — Event Organizer Department of PPI Aachen 📋\nThe ones who make sure every detail is in place, every timing is right, and everything runs exactly as it should",
                     "members": [
-                        "Nicholas Daniel Tjandrasa (Ketua)",
-                        "Renata Shannon Rijanto",
-                        "Delix Andea",
-                        "Filbert Filbert",
-                        "Joan Ivanka",
-                        "Jesslyn Aurelia Tjen",
-                        "Bonfilio Onassis Setiawan"
-                    ]
-                },
-                {
-                    "name": "Fundraising",
-                    "description": "Buy our merch pls 👉👈. We promise it’s cute and for a good cause 😌. From designing must-have merch to organizing creative fundraising events, they’re the financial fuel behind PPI Aachen’s journey this year.",
-                    "members": [
-                        "Liorio Anargya Putra Wibowo (Ketua)",
-                        "Imelda Halim",
-                        "Grace Adeline",
-                        "Philipus Dean Laurent",
-                        "Kenzie Weili Wangkawa Winarto",
-                        "Keanu Rafif Shadiq Hidayat",
-                        "James Edward"
-                    ]
-                },
-                {
-                    "name": "Education",
-                    "description": "We’re not nerds 😤. We’re just passionately smart 🤓. From academic enrichment to insightful workshops, they’re here to empower, inspire, and support your learning journey throughout the year.",
-                    "members": [
-                        "Brendon Arie Putra (Ketua)",
-                        "Jason Louis Laksono",
-                        "Sheifer Sidney Cornelly",
-                        "Timothy Setiawan",
-                        "Dheo Santoso",
-                        "Ahmad Maulana",
-                        "Ghithrafi Fadhgazka Syarif"
-                    ]
-                },
-                {
-                    "name": "Multimedia and IT",
-                    "description": "Our eyes may hurt after this period 👁️💧👄💧👁️. From content creation to tech support, they’ll be behind all things visual and digital for PPI Aachen!",
-                    "members": [
-                        "Bartholomeus Marciano Nathan (Ketua)",
-                        "Michael Xhristiano Espranata",
-                        "Rafael Billy Jayadi",
-                        "Deronn Jehosaphat",
-                        "Fachrial Dimas Putra Perdana",
-                        "Matthew Nathaniel Panggabean",
-                        "Navy Hanan",
-                        "Samuel Raphael Tanos",
-                        "Olivier Al Drich"
+                        "Riovalen Wijaya",
+                        "Izz Kiram Prastowo",
+                        "Samuel Rowenville Krezenskoei"
                     ]
                 }
             ]
